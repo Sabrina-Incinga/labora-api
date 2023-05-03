fafead623a7cd9b6caa9213ee241a57b04ea0cdf
Sabrina-Incinga <sabrinagincinga@gmail.com>
2023-05-03 00:01:04 -0300

    sql, primer script

---
+++ sql/create_items.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE labora_proyect_1;
+
+CREATE TABLE items(
+id INT PRIMARY KEY,
+customer_name VARCHAR(100) NOT NULL,
+order_date DATE NOT NULL,
+product VARCHAR(255) NOT NULL,
+quantity INT NOT NULL,
+price NUMERIC NOT NULL)
